c9b41cea1dcdb052748044c20ce2d7966300aac0
WX <123456>
2024-08-01 17:58:59 +0800

    feat: 加一些代码注释

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -42,7 +42,8 @@ export function getToken(): DataInfo<number> {
 
 /**
  * @description 设置`token`以及一些必要信息并采用无感刷新`token`方案
- * 无感刷新：后端返回`accessToken`（访问接口使用的`token`）、`refreshToken`（用于调用刷新`accessToken`的接口时所需的`token`，`refreshToken`的过期时间（比如30天）应大于`accessToken`的过期时间（比如2小时））、`expires`（`accessToken`的过期时间）
+ * 无感刷新：后端返回`accessToken`（访问接口使用的`token`）、`refreshToken`
+ * （用于调用刷新`accessToken`的接口时所需的`token`，`refreshToken`的过期时间（比如30天）应大于`accessToken`的过期时间（比如2小时））、`expires`（`accessToken`的过期时间）
  * 将`accessToken`、`expires`、`refreshToken`这三条信息放在key值为authorized-token的cookie里（过期自动销毁）
  * 将`avatar`、`username`、`nickname`、`roles`、`refreshToken`、`expires`这六条信息放在key值为`user-info`的localStorage里（利用`multipleTabsKey`当浏览器完全关闭后自动销毁）
  */
@@ -71,16 +72,27 @@ export function setToken(data: DataInfo<Date>) {
   );
   // 设置用户信息
   function setUserKey({ avatar, username, nickname, roles }) {
+    // 更新用户头像
     useUserStoreHook().SET_AVATAR(avatar);
+    // 更新用户名
     useUserStoreHook().SET_USERNAME(username);
+    // 更新用户昵称
     useUserStoreHook().SET_NICKNAME(nickname);
+    // 更新用户角色
     useUserStoreHook().SET_ROLES(roles);
+    // 将用户信息存储到本地存储中
     storageLocal().setItem(userKey, {
+      // 存储刷新令牌
       refreshToken,
+      // 存储过期时间
       expires,
+      // 存储用户头像
       avatar,
+      // 存储用户名
       username,
+      // 存储用户昵称
       nickname,
+      // 存储用户角色
       roles
     });
   }
@@ -112,10 +124,15 @@ export function setToken(data: DataInfo<Date>) {
   }
 }
 
-/** 删除`token`以及key值为`user-info`的localStorage信息 */
+/**
+ * 删除`token`以及key值为`user-info`的localStorage信息
+ */
 export function removeToken() {
+  // 删除 Cookies 中的 TokenKey
   Cookies.remove(TokenKey);
+  // 删除 Cookies 中的 multipleTabsKey
   Cookies.remove(multipleTabsKey);
+  // 删除 localStorage 中的 userKey
   storageLocal().removeItem(userKey);
 }
 
